0872f98c081365e96c336c7875200ff03504d9cc
Sharad8518 <sharadkum9@gmail.com>
2025-09-13 01:46:39 +0530

    link change

--- src/component/api/axios.js
+++ src/component/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-   baseURL: "http://localhost:4000/api", // your backend URL
-  // baseURL:"https://houseofziba-nodejs.onrender.com/api", // your backend URL
+  //  baseURL: "http://localhost:4000/api", // your backend URL
+  baseURL:"https://houseofziba-nodejs.onrender.com/api", // your backend URL
 });
 
 // No token interceptor
